cdbadc98b6acb5b1275c9c7af6b7e3796898e7d7
OSTERITE <adrian@bandidos.org>
2023-01-11 20:18:28 +0100

    Optimization and code cleanup, less direct calling to DOM, which makes getting color faster,

--- color_function/script.js
+++ color_function/script.js
@@ -1,28 +1,18 @@
 
-//------------------------------\\
-//math.js library is recommended
-//npm install mathjs
-//------------------------------\\
-
-//struktur rekkefølge:
-//Klasser, globale variabler
-//window.onload = winInit
-//også Funskjoner sikkert
-
-
 //---------------------Accessing DOM elements------------------------
 
 const get_hue_expression = document.getElementById("hue_expression");
 const get_saturation_expression = document.getElementById("saturation_expression");
-const get_lightness_expression = document.getElementById("lightness_expression");
+const get_lightness_expression = document.getElementById( "lightness_expression" );
 const get_size_lower = document.getElementById("size_lower");
 const get_size_upper = document.getElementById("size_upper");
 const get_pixel_size = document.getElementById("pixel_size");
 const get_upscale = document.getElementById("upscale");
 
-get_hue_expression.addEventListener("change", function () { hsl_loop(1); });
-get_saturation_expression.addEventListener("change", function () { hsl_loop(2); });
-get_lightness_expression.addEventListener("change", function () { hsl_loop(3); });
+get_hue_expression.addEventListener("change", function () { hue_expression = get_hue_expression.value; hsl_loop(1); });
+get_saturation_expression.addEventListener("change", function () { saturation_expression = get_saturation_expression.value; hsl_loop(2); });
+get_lightness_expression.addEventListener("change", function () { lightness_expression = get_lightness_expression.value; hsl_loop(3); });
+
 get_size_lower.addEventListener("change", change_size_lower);
 get_size_upper.addEventListener("change", change_size_upper);
 get_pixel_size.addEventListener("change", change_pixel_size);
@@ -37,9 +27,8 @@ get_upscale.addEventListener("click", function () {
 //-----------------------Canvas-----------------------------
 
 let canvas = document.getElementById("canvas");
-canvas.addEventListener("mousedown", function (e) {get_cursor_position(canvas, e);});
-canvas.addEventListener("mouseup", function (e) {get_cursor_position(canvas, e);});
-
+canvas.addEventListener("mousedown", function (e) { get_cursor_position(canvas, e); });
+canvas.addEventListener("mouseup", function (e) { get_cursor_position(canvas, e); })
 const ctx = canvas.getContext("2d", { alpha: false });
 ctx.imageSmoothingEnabled = false;
 // ctx.imageSmoothingQuality = "high"
@@ -50,14 +39,16 @@ let matrix_pixels = [];
 let size_lower = +get_size_lower.value;
 let size_upper = +get_size_upper.value;
 let size = (Math.abs(size_lower) + size_upper) / pixel_size;
-let absolute_width
+let absolute_width;
 var pixel_size = parseFloat(get_pixel_size.value);
-
-
 
 //Distance from top left corner for x and y values
 let distance_x = size_lower;
 let distance_y = size_lower;
+
+let hue_expression = get_hue_expression.value
+let saturation_expression = get_saturation_expression.value
+let lightness_expression = get_lightness_expression.value
 
 //-------------------------------GUIDING BOX FOR RESIZE--------------------
 
@@ -88,7 +79,6 @@ class pixel {
 
   tegn() {
     if (isFinite(this.hue)) {
-      
       draw(
         this.xpos,
         this.ypos,
@@ -96,12 +86,12 @@ class pixel {
         this.pixel_size,
         `hsl( ${this.hue} , ${this.saturation}% , ${this.lightness}%)`
       );
-
-    } else {
+    } 
+    else {
       draw(
-      this.xpos,
-      this.ypos,
-      this.pixel_size,
+        this.xpos,
+        this.ypos,
+        this.pixel_size,
         this.pixel_size,
         `hsl(0, 0%, 0%)`
       );
@@ -110,29 +100,18 @@ class pixel {
   }
 
   hue_changed(x, y) {
-    var hue_expression = get_hue_expression.value
-      .replace(/X/g, x)
-      .replace(/Y/g, y);
-    this.hue = Function(`return ${hue_expression}`)();
+    this.hue = Function(`return ${hue_expression.replace(/X/g, x).replace(/Y/g, y)}`)();
     this.tegn();
   }
 
   saturation_changed(x, y) {
-    var saturation_expression = get_saturation_expression.value
-      .replace(/X/g, x)
-      .replace(/Y/g, y);
     this.saturation = Math.abs(
-      ((100 + Function(`return ${saturation_expression}`)()) % 200) - 100
-    );
+      ((100 + Function(`return ${saturation_expression.replace(/X/g, x).replace(/Y/g, y)}`)()) % 200) - 100);
     this.tegn();
   }
   lightness_changed(x, y) {
-    var lightness_expression = get_lightness_expression.value
-      .replace(/X/g, x)
-      .replace(/Y/g, y);
     this.lightness = Math.abs(
-      ((100 + Function(`return ${lightness_expression}`)()) % 200) - 100
-    );
+      ((100 + Function(`return ${lightness_expression.replace(/X/g, x).replace(/Y/g, y)}`)()) % 200) - 100);
     this.tegn();
   }
 }
@@ -144,7 +123,6 @@ function winInit() {
   // ctx.filter = "hue-rotate(200deg)" INTERESTING!
   size = (Math.abs(size_lower) + size_upper) / pixel_size;
   absolute_width = canvas.width / size; //width in px of every "pixel" drawn on canvas
-
   new_pixels(size_lower, size_lower, size_upper, size_upper);
 }
 
@@ -163,18 +141,18 @@ function draw(x, y, width, heigth, color) {
 function hsl_loop(letter) {
   if (letter === 1) {
     var letter_method = pixel.prototype.hue_changed;
-  } else if (letter === 2) {
+  } 
+  else if (letter === 2) {
     var letter_method = pixel.prototype.saturation_changed;
-  } else if (letter === 3) {
+  } 
+  else if (letter === 3) {
     var letter_method = pixel.prototype.lightness_changed;
   }
-
 
   distance_x = size_lower;
   distance_y = size_lower;
 
   absolute_width = canvas.width / size; //width in px of every "pixel" drawn on canvas
-
 
   for (let x = size_lower; x < size_upper; x++) {
     for (let y = size_lower; y < size_upper; y++) {
@@ -182,66 +160,58 @@ function hsl_loop(letter) {
     }
   }
   dataURL = canvas.toDataURL();
-  resizing_img.src = dataURL;
   original_img.src = dataURL;
+  resizing_img.src = dataURL;
 }
 
 function create_pixels(start, end) {
   size = (Math.abs(size_lower) + size_upper) / pixel_size;
-
   new_pixels(start, start, end, end);
 }
 
-function draw_pixels(
-  dimension_start_x,
-  dimension_end_x,
-  dimension_start_y,
-  dimension_end_y
-) {
+function draw_pixels(dimension_start_x, dimension_end_x, dimension_start_y, dimension_end_y) {
   for (let x = dimension_start_x, width = dimension_end_x; x < width; x++) {
     for (let y = dimension_start_y, length = dimension_end_y; y < length; y++) {
       matrix_pixels[x][y].tegn();
     }
   }
+  resizing_img.src = canvas.toDataURL();
 }
 
 function change_hue(x, y) {
-  let returnme = get_hue_expression.value.replace(/X/g, x).replace(/Y/g, y);
-  return Function(`return ${returnme}`)();
+  return Function(`return ${hue_expression.replace(/X/g, x).replace(/Y/g, y)}`)();
 }
 
 function change_saturation(x, y) {
-  let returnme = get_saturation_expression.value
-    .replace(/X/g, x)
-    .replace(/Y/g, y);
-  return Math.abs(((100 + Function(`return + ${returnme}`)()) % 200) - 100);
+  //Sawtooth pattern
+  return Math.abs(((100 + Function(`return + ${saturation_expression.replace(/X/g, x).replace(/Y/g, y)}`)()) % 200) - 100);
 }
 
 function change_lightness(x, y) {
-  let returnme = get_lightness_expression.value
-    .replace(/X/g, x)
-    .replace(/Y/g, y);
-  return Math.abs(((100 + Function(`return + ${returnme}`)()) % 200) - 100);
+  return Math.abs(((100 + Function(`return + ${lightness_expression.replace(/X/g, x).replace(/Y/g, y)}`)()) % 200) - 100);
 }
 
 //TODO: turn into 1 function... ?
 
 function change_size_upper() {
-  var new_size = parseInt(get_size_upper.value);
+  let new_size = parseInt(get_size_upper.value);
 
   switch (true) {
+    
     case new_size > size_upper:
-      var old_size_upper = size_upper;
+      let old_size_upper = size_upper;
       size_upper = new_size;
       size = (Math.abs(size_lower) + size_upper) / pixel_size;
+      
       distance_x = size_lower;
       distance_y = size_lower;
+      
       ctx.drawImage(
         resizing_img,
         0,
         0,
-        ((600 / size) * (size - (new_size - old_size_upper))).toFixed(4),
-        ((600 / size) * (size - (new_size - old_size_upper))).toFixed(4)
+        ~~((600 / size) * (size - (new_size - old_size_upper))),
+        ~~((600 / size) * (size - (new_size - old_size_upper)))
       );
 
       new_pixels(size_lower, old_size_upper, size_upper, size_upper);
@@ -257,11 +227,11 @@ function change_size_upper() {
 }
 
 function change_size_lower() {
-  var new_size = parseInt(get_size_lower.value) / pixel_size;
+  let new_size = parseInt(get_size_lower.value) / pixel_size;
 
   switch (true) {
     case new_size < size_lower:
-      var old_size_lower = size_lower;
+      let old_size_lower = size_lower;
       size_lower = new_size;
 
       size = Math.abs(new_size) + size_upper;
@@ -269,8 +239,8 @@ function change_size_lower() {
         resizing_img,
         (600 / size) * (old_size_lower - new_size),
         (600 / size) * (old_size_lower - new_size),
-        ((600 / size) * (size - (old_size_lower - new_size))).toFixed(4),
-        ((600 / size) * (size - (old_size_lower - new_size))).toFixed(4)
+        ~~((600 / size) * (size - (old_size_lower - new_size))),
+        ~~((600 / size) * (size - (old_size_lower - new_size)))
       );
 
       distance_x = size_lower;
@@ -293,7 +263,8 @@ function change_pixel_size() {
   //Because it makes pixel_size a string and not a float
   if (get_pixel_size.value.includes('"')) {
     pixel_size = get_pixel_size.value.replace(/\"/g, "");
-  } else {
+  } 
+  else {
     pixel_size = parseFloat(get_pixel_size.value);
   }
 
@@ -307,56 +278,51 @@ function get_cursor_position(canvas, event) {
   size = Math.abs(size_lower) + size_upper;
 
   let absolute_width_pixel = canvas.width / size;
-  // var absolute_heigth_pixel = canvas.height / size;
+  // let absolute_heigth_pixel = canvas.height / size;
 
   if (event.type == "mousedown") {
     canvas.addEventListener("mousemove", zoom_guider);
 
     //finds the absolute coordinates clicked
-    var down_x = event.offsetX;
-    var down_y = event.offsetY;
+    let down_x = event.offsetX;
+    let down_y = event.offsetY;
 
     clicked_released_xpos = [down_x];
     clicked_released_ypos = [down_y];
-  } else if (event.type == "mouseup") {
+  } 
+  
+  else if (event.type == "mouseup") {
     canvas.removeEventListener("mousemove", zoom_guider);
 
     if (event.ctrlKey) {
-
       ctx.drawImage(original_img, 0, 0, 600, 600);
-      dataURL = canvas.toDataURL();
-      resizing_img.src = dataURL;
+      resizing_img.src = canvas.toDataURL();
       return;
-    } else {
-      clicked_released_xpos.sort(function (a, b) {
-        return a - b;
-      });
-      clicked_released_ypos.sort(function (a, b) {
-        return a - b;
-      });
+    } 
+    
+    else {
+      //sorts array from lowest to highest
+      clicked_released_xpos.sort(function (a, b) {return a - b;});
+      clicked_released_ypos.sort(function (a, b) {return a - b;});
 
-      var start_x =
-        ~~(clicked_released_xpos[0] / absolute_width_pixel) + size_lower;
-      var end_x =
-        ~~(clicked_released_xpos[1] / absolute_width_pixel) + size_lower;
-
-      var start_y =
-        ~~(clicked_released_ypos[0] / absolute_width_pixel) + size_lower;
-      var end_y =
-        ~~(clicked_released_ypos[1] / absolute_width_pixel) + size_lower;
+      let start_x = ~~(clicked_released_xpos[0] / absolute_width_pixel) + size_lower;
+      let end_x   = ~~(clicked_released_xpos[1] / absolute_width_pixel) + size_lower;
+      let start_y = ~~(clicked_released_ypos[0] / absolute_width_pixel) + size_lower;
+      let end_y   = ~~(clicked_released_ypos[1] / absolute_width_pixel) + size_lower;
 
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       if (end_x - start_x > end_y - start_y) {
         start_y -= 1;
-      } else if (end_x - start_x < end_y - start_y) {
+      } 
+      else if (end_x - start_x < end_y - start_y) {
         start_x -= 1;
       }
 
       size = end_x - start_x + 1;
       distance_x = start_x;
       distance_y = start_y;
-      absolute_width = canvas.width / size
+      absolute_width = canvas.width / size;
       draw_pixels(start_x, end_x + 1, start_y, end_y + 1);
     }
   }
@@ -371,14 +337,13 @@ function zoom_guider() {
   clicked_released_xpos[1] = current_x;
   clicked_released_ypos[1] = current_y;
   ctx.beginPath();
-  var subtract;
 
-  var height;  //Positive values down, negative values up.
-  var width;   //positive values to the right, negative to the left.
+  let height; //Positive values down, negative values up.
+  let width; //positive values to the right, negative to the left.
 
   //checks where current x and y pos are in relation to down_x and down_y
-  var right = false;
-  var above = false;
+  let right = false;
+  let above = false;
 
   if (current_x - clicked_released_xpos[0] > 0) {
     right = true;
@@ -389,57 +354,46 @@ function zoom_guider() {
   }
 
   if ((right && above) || (!right && !above)) {
-    if (
-      Math.abs(current_x - clicked_released_xpos[0]) >
-      Math.abs(current_y - clicked_released_ypos[0])
-    ) {
+    
+    //FIXME: Super chunky and ugly
+    if (Math.abs(current_x - clicked_released_xpos[0]) > Math.abs(current_y - clicked_released_ypos[0])) {
       width = current_x - clicked_released_xpos[0];
       height = -(current_x - clicked_released_xpos[0]);
-    } else {
-      subtract = clicked_released_ypos[0];
-      width = -(current_y - subtract);
-      height = current_y - subtract;
+    } 
+    else {
+      width = -(current_y -  clicked_released_ypos[0]);
+      height = current_y -  clicked_released_ypos[0];
     }
-  } else if ((right && !above) || (!right && above)) {
-    if (
-      Math.abs(current_x - clicked_released_xpos[0]) >
-      Math.abs(current_y - clicked_released_ypos[0])
-    ) {
+  } 
+  else if ((right && !above) || (!right && above)) {
+    
+    if (Math.abs(current_x - clicked_released_xpos[0]) > Math.abs(current_y - clicked_released_ypos[0])) {
       width = current_x - clicked_released_xpos[0];
       height = current_x - clicked_released_xpos[0];
-    } else {
+    } 
+    else {
       width = current_y - clicked_released_ypos[0];
       height = current_y - clicked_released_ypos[0];
     }
   }
-  //TODO: Redundant to have both width and heigth ?
 
   //Draws the guiding box
   clicked_released_xpos[1] = clicked_released_xpos[0] + ~~width;
   clicked_released_ypos[1] = clicked_released_ypos[0] + ~~height;
   ctx.rect(clicked_released_xpos[0], clicked_released_ypos[0], width, height);
-  ctx.stroke(); 
+  ctx.stroke();
 }
 
-function new_pixels(dimension_start_x,
-  dimension_start_y,
-  dimension_width,
-  dimension_length
-) {
-  absolute_width = canvas.width / size
-
+function new_pixels(start_x, start_y, width, length) {
+  absolute_width = canvas.width / size;
   //column
-  //width is locally declared as dimension_width for improved performance by reducing amount of property lookups
-  for (let x = dimension_start_x, width = dimension_width; x < width; x++) {
+  //width is locally declared as width for improved performance by reducing amount of property lookups
+  for (let x = start_x, runs = width; x < runs; x++) {
     if (matrix_pixels[x] == undefined) {
-      matrix_pixels[x] = new Array(~~dimension_length);
+      matrix_pixels[x] = new Array(~~length);
     }
 
-    for (
-      let y = dimension_start_y, length = dimension_length;
-      y < length;
-      y++
-    ) {
+    for (let y = start_y, runs = length; y < runs; y++) {
       matrix_pixels[x][y] = new pixel(
         x * pixel_size,
         y * pixel_size,
@@ -451,24 +405,20 @@ function new_pixels(dimension_start_x,
     }
   }
 
-  if (
-    dimension_start_x == dimension_start_y &&
-    dimension_width == dimension_length
-  ) {
+  if (start_x == start_y && width == length) {
     dataURL = canvas.toDataURL();
-    resizing_img.src = dataURL;
     original_img.src = dataURL;
+    resizing_img.src = dataURL;
     return;
   }
 
   // row
-  for (let x = dimension_start_y; x < dimension_length; x++) {
+  for (let x = start_y, runs = length; x < runs; x++) {
     if (matrix_pixels[x] == undefined) {
-      matrix_pixels[x] = [];
-      // matrix_pixels[x] = new Array(~~(dimension_width));
+      matrix_pixels[x] = new Array(~~width);
     }
 
-    for (let y = dimension_start_x; y < dimension_width; y++) {
+    for (let y = start_x, runs = width; y < runs; y++) {
       matrix_pixels[x][y] = new pixel(
         x * pixel_size,
         y * pixel_size,
@@ -479,25 +429,10 @@ function new_pixels(dimension_start_x,
       );
     }
   }
-
-  // resizing_img = new Image();
-  dataURL = canvas.toDataURL();
-  resizing_img.src = dataURL;
-
-  // original_img = new Image();
   dataURL = canvas.toDataURL();
   original_img.src = dataURL;
+  resizing_img.src = dataURL;
 }
-
-
-
-// Fn =
-// (((1 + Math.sqrt(5)) / 2) ** (X+Y) - ((1 - Math.sqrt(5)) / 2) ** (X+Y)) /
-// Math.sqrt(5);
-
-// console.log(math.i.re, "real")
-// console.log(math.i.im, "imaginary")
-// console.log(math.multiply(math.i, 10).im, "imaginary")
 
 //------------------START--------------------
 //TO BE USED ANOTHER TIME?
@@ -522,8 +457,6 @@ function new_pixels(dimension_start_x,
 
 //!HUGE? TODO: use webworkers, ask chat gpt-3 for help
 
-//!HUGE TODO: create own functions and such instead of using fulabl libraries.
-//! Functions to be made self include : tegnFyltRektangel, tegnfirkant, tegnBrukXY, tegnBrukBakgrunn, tegnBrukSynsfelt, tegnBrukCanvas
 
 //TODO: Minor fix in the new_pixels function, it creates the corner piece twice
 
@@ -558,3 +491,4 @@ function new_pixels(dimension_start_x,
 //* Fix: dont need to make new Image() everytime resizing_img is declared or whatever
 //* No point in drawing everything of only a small part is shown,
 //* make it so that you can only draw complete pixels with zoom_guider, and only draw and show the pixels "selected"
+//* HUGE create own functions and such instead of using fulabl libraries. Functions to be made self include : tegnFyltRektangel, tegnfirkant, tegnBrukXY, tegnBrukBakgrunn, tegnBrukSynsfelt, tegnBrukCanvas
